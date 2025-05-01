648f7e548188eb9a3a6b0a9887e10f6b4d376aa1
Cem Serimer <cem.serimer@tedu.edu.tr>
2025-05-02 00:44:22 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <complex.h>
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int thread_count = 1;
+int signal_size = 4096;
+
+typedef complex double cplx;
+
+void parallel_fft(cplx x[], int N);
+void fft(cplx x[], int N, int s);
+void generate_data(cplx* x, int N);
+
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        signal_size = atoi(argv[1]);
+        if (argc == 3) {
+            thread_count = atoi(argv[2]);
+        }
+    } else {
+        thread_count = omp_get_max_threads();
+    }
+
+    printf("1\n");
+    cplx* data = (cplx*)malloc(signal_size * sizeof(cplx));
+    printf("2\n");
+    generate_data(data, signal_size);
+    printf("3\n");
+
+    /*
+    printf("Original data: ");
+    for (int i = 0; i < signal_size; i++) {
+        if (cimag(data[i]) == 0)
+            printf("%.2f ", creal(data[i]));
+        else
+            printf("%.2f%+.2fi ", creal(data[i]), cimag(data[i]));
+    }
+    printf("\n");
+    */
+    printf("Starting\n");
+    parallel_fft(data, signal_size);
+
+    /*
+    printf("FFT result: ");
+    for (int i = 0; i < signal_size; i++) {
+        printf("%.2f%+.2fi ", creal(data[i]), cimag(data[i]));
+    }
+    printf("\n");
+    */
+    printf("Done\n");
+    return 0;
+}
+
+void parallel_fft(cplx x[], int N) { fft(x, N, 1); }
+
+void fft(cplx x[], int N, int s) {
+    if (N == 1) {
+        return;
+    } else {
+        fft(x, N / 2, 2 * s);
+        fft(x + s, N / 2, 2 * s);
+
+        for (int k = 0; k < N / 2; k++) {
+            cplx p = x[k * 2 * s];
+            cplx q = cexp(-2 * M_PI * I * k / N) * x[k * 2 * s + s];
+            x[k * 2 * s] = p + q;
+            x[k * 2 * s + s] = p - q;
+        }
+    }
+}
+
+void generate_data(cplx* x, int N) {
+    srand(time(NULL));
+#pragma omp parallel for num_threads(thread_count)
+    for (int i = 0; i < N; i++) {
+        x[i] = rand();
+    }
+}
