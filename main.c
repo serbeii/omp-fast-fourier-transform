c925cac02a30813b3597373ad4c38b6986c54690
Cem Serimer <cem.serimer@tedu.edu.tr>
2025-05-02 03:19:10 +0300

    Added task level parallelization

--- main.c
+++ main.c
@@ -6,7 +6,10 @@
 #include <time.h>
 
 int thread_count = 1;
-int signal_size = 4096;
+
+#define FFT_TASK_CUTOFF 4096
+#define FFT_LOOP_CUTOFF 512
+// #define DEBUG
 
 typedef complex double cplx;
 
@@ -15,6 +18,7 @@ void fft(cplx x[], int N, int s);
 void generate_data(cplx* x, int N);
 
 int main(int argc, char** argv) {
+    int signal_size = 4096;
     if (argc > 1) {
         signal_size = atoi(argv[1]);
         if (argc == 3) {
@@ -30,7 +34,7 @@ int main(int argc, char** argv) {
     generate_data(data, signal_size);
     printf("3\n");
 
-    /*
+#ifdef DEBUG
     printf("Original data: ");
     for (int i = 0; i < signal_size; i++) {
         if (cimag(data[i]) == 0)
@@ -39,30 +43,43 @@ int main(int argc, char** argv) {
             printf("%.2f%+.2fi ", creal(data[i]), cimag(data[i]));
     }
     printf("\n");
-    */
+#endif
     printf("Starting\n");
     parallel_fft(data, signal_size);
 
-    /*
+#ifdef DEBUG
     printf("FFT result: ");
     for (int i = 0; i < signal_size; i++) {
         printf("%.2f%+.2fi ", creal(data[i]), cimag(data[i]));
     }
     printf("\n");
-    */
+#endif
     printf("Done\n");
     return 0;
 }
 
-void parallel_fft(cplx x[], int N) { fft(x, N, 1); }
+void parallel_fft(cplx x[], int N) {
+#pragma omp parallel
+    {
+#pragma omp single
+        fft(x, N, 1);
+    }
+}
 
 void fft(cplx x[], int N, int s) {
     if (N == 1) {
         return;
     } else {
-        fft(x, N / 2, 2 * s);
-        fft(x + s, N / 2, 2 * s);
-
+        if (N > FFT_TASK_CUTOFF) {
+#pragma omp task
+            fft(x, N / 2, 2 * s);
+#pragma omp task
+            fft(x + s, N / 2, 2 * s);
+#pragma omp taskwait
+        } else {
+            fft(x, N / 2, 2 * s);
+            fft(x + s, N / 2, 2 * s);
+        }
         for (int k = 0; k < N / 2; k++) {
             cplx p = x[k * 2 * s];
             cplx q = cexp(-2 * M_PI * I * k / N) * x[k * 2 * s + s];
